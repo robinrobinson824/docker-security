c8faf56664e045836aee0a0f5979fbf9d2295feb
Robin <robinrobinsonaus@outlook.com>
2025-05-26 18:29:40 +1000

    calc webpage

---
+++ old_calc.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+    "fmt"
+    "net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprint(w, `<html>
+        <head><title>Hello</title></head>
+        <body style="background-color: purple; display: flex; align-items: center; justify-content: center; height: 100vh;">
+            <h1 style="color: white;">Hello, World!</h1>
+        </body>
+    </html>`)
+}
+
+func main() {
+    http.HandleFunc("/", handler)
+    fmt.Println("Server running on http://localhost:8080")
+    http.ListenAndServe(":8080", nil)
+}
